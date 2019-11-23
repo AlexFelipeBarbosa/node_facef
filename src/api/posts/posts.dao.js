bd3f53b41c42456cbcd6495de2161cdbac28e037
ALEX FELIPE BARBOSA <22289@unifacef.edu.br>
2019-11-23 15:16:55 -0300

    Criando os TAGS

--- src/api/posts/posts.dao.js
+++ src/api/posts/posts.dao.js
@@ -6,4 +6,23 @@ export default class PostsDAO {
     findAll() {
         return Post.findAll();
     }
+
+    findById(id) {
+        return Post.findByPk(id);
+    }
+
+    create (data) {
+    return Post.create(data);
+    }
+
+    async update(id, post) {
+        await Post.update(post, {where: { id }});
+        return await this.findById(id);
+    }
+
+    destroy(id) {
+        return Post.destroy ({where: { id }});
+    }
+
+
 }
